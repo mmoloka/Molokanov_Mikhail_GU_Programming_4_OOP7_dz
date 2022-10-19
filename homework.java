957872c95c0bf3a4f11dc48e38704a968e59eb1b
«Mikhail» <kopfgolova@gmail.com>
2022-10-19 19:07:57 +0300

    Initial commit

---
+++ homework.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+/*Написать консольный калькулятор принимающий два числа и операто(умножение, деление, сложение, вычитание) и завершающий 
+процесс по решению пользователя. Завершающее слово должно быть не регистрочувствительным(Пример: поданные слова в разном
+ регистре "Да", "да", "дА", "ДА" - должны быть однозначно трактованны, как "Да").
+Пример:
+Введите первое число:
+5
+Введите второе число:
+10
+Введите желаемую операцию
+*
+Результат: 50
+Хотите продолжить?
+Нет */
+public class homework {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int flag = 0;
+        while(flag == 0){
+            System.out.println("Введите первое число: ");
+            int num1 = sc.nextInt();
+            System.out.println("Введите второе число: ");
+            int num2 = sc.nextInt();
+            System.out.println("Введите желаемую операцию: ");
+            String operat = sc.next();
+            switch(operat){
+                case "+":
+                System.out.printf("Результат: %d", num1 + num2);
+                break;
+                case "-":
+                System.out.printf("Результат: %d", num1 - num2);
+                break;
+                case "*":
+                System.out.printf("Результат: %d", num1 * num2);
+                break;
+                case "/":
+                System.out.printf("Результат: %d", num1 / num2);
+                break;
+            }
+            System.out.println("\nХотите продолжить ?");
+            String answer = sc.next();
+            switch(answer.toUpperCase()){
+                case "NO":
+                flag++;
+                break;
+            }
+        }
+        sc.close();
+
+    }
+}
